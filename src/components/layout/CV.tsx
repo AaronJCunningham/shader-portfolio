b1e50c87c9dfd4243eebd7b4445d76c8ff45fdac
aaron-montra <aaron@montra.co>
2024-01-16 14:19:51 +0100

    new bio

--- src/components/layout/CV.tsx
+++ src/components/layout/CV.tsx
@@ -3,55 +3,37 @@ export const CV = () => {
     <div id="bio" className="bio_content">
       <h2 className="bio-h2">ABOUT ME</h2>
       <p>
-        As a frontend developer, my expertise lies in the development of web3
-        and metaverse applications. With a unique blend of artistic and
-        technical skills, I excel at crafting highly engaging online
-        experiences.
+      I am deeply inspired by the intersection of art and technology, particularly in emerging technologies.
       </p>
       <p>
-        I love building immersive web apps with Three.js and @react-three/fiber
-        inside Next.js and React.
+      My main area of expertise as a frontend developer involves creating interactive, immersive websites, focusing on metaverses and 3D animations. My interest in web3 has been a significant part of my 10-year career in the blockchain industry.
       </p>
       <p>
-        Throughout my career, I have worked for several prominent blockchain
-        companies, including Coinsquare, Canada&apos;s largest cryptocurrency
-        exchange, and IOST, a proof-of-stake blockchain platform based in Asia.
+      My primary tech stack includes TypeScript, Next.js, React, and Three.js, but I am flexible and excited to learn other technologies.
       </p>
       <p>
-        Currently, my focus is on web3 and browser-based metaverse development.
-        In 2022 I helped RaveSpace, one of the world&apos;s leading metaverse
-        development studios, launch their Musee Dezentral project, a museum
-        dedicated to NFTs.
+      In 2023, I served as the lead Three.js developer at <a href="https://montra.com" target="_blank" rel="noopener noreferrer">Montra</a>
+, a web-based video editing startup.
       </p>
-
       <p>
-        Now, I run my own{" "}
-        <a href="https://xeleven.tech" target="_blank" rel="noreferrer">
-          metaverse studio XELEVEN
-        </a>
-        , where we specialize in building immersive 3D metaverse sites for
-        clients such as{" "}
-        <a
-          href="https://theface.com/tiffany-calvers-world"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nike
-        </a>
-        ,{" "}
-        <a href="https://houseofpersona.com" target="_blank" rel="noreferrer">
-          House Of Persona
-        </a>
-        , and{" "}
-        <a href="https://bauhauserde.org" target="_blank" rel="noreferrer">
-          Bauhaus Earth
-        </a>
-        . We recently built our own metaverse called the{" "}
-        <a href="https://spiritrealm.art" target="_blank" rel="noreferrer">
-          Spirit Realm
-        </a>
-        . I believe that Spirit Realm best represents what I am capable of
-        creating.
+      Between 2020 and 2023, I was the lead developer at <a href="https://xeleven.tech" target="_blank" rel="noopener noreferrer">XELEVEN</a>, a metaverse development studio, where we undertook projects for notable clients like <a href="https://theface.com/tiffany-calvers-world" target="_blank" rel="noopener noreferrer">NIKE.</a> Our metaverse, <a href="https://www.spiritrealm.art" target="_blank" rel="noopener noreferrer">Spirit Realm,</a>
+ was featured at NFT NYC, Times Square NYC, and Red-Eye metaverse magazine.
+      </p>
+      <p>
+      In 2022, I contributed to <a href="https://ravespace.io" target="_blank" rel="noopener noreferrer">RaveSpace&apos;s </a>
+ launch of the Musee Dezentral project, a museum dedicated to NFTs.
+      </p>
+      <p>
+      Throughout my career, I have been part of several prominent blockchain companies, including <a href="https://coinsquare.com" target="_blank" rel="noopener noreferrer">Coinsquare</a>
+, Canada's largest cryptocurrency exchange, and <a href="https://iost.io" target="_blank" rel="noopener noreferrer">IOST</a>
+ in Asia.
+      </p>
+      <p>
+      I have given talks and shared my insights on blockchain technology at venues like <a href="https://www.aaronjcunningham.com/about/chainlink-in-berlin" target="_blank" rel="noopener noreferrer">Chainlink HQ in Berlin</a>
+ and the Google Campus in Warsaw.
+      </p>
+      <p>
+      I also have a passion for music. I have co-written 5 songs that reached the top 20 on the Billboard charts and have been featured in VICE, BoilerRoom, and BlogTO. I am also a three-time Prism Prize Award nominee.
       </p>
     </div>
   );
