a757acb9c05d430842839902511f84fad5bf1857
aaron-montra <aaron@montra.co>
2024-01-18 12:57:13 +0100

    new preview image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'Home - Aaron J. Cunningham',
   description: "Aaron J. Cunningham is a frontend developer specializing in metaverse & web3 using Three.js, R3F, Next.js & React.",
   openGraph: {
-    images: 'https://ik.imagekit.io/fx30u3wgcqib/desktop_J3mnPtJoj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1675814941001',
+    images: 'https://ik.imagekit.io/fx30u3wgcqib/Screenshot%202024-01-18%20124209_v5n4oEu5V.png?updatedAt=1705578827046',
   },
   
 }
--- src/components/metadata/MetaDataHeader.js
+++ src/components/metadata/MetaDataHeader.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 
 const MetaDataHeader = ({
   content = "Aaron J. Cunningham is a frontend developer specializing in metaverse & web3 using Three.js, R3F, Next.js & React.",
-  image = "https://ik.imagekit.io/fx30u3wgcqib/desktop_J3mnPtJoj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1675814941001",
+  image = "https://ik.imagekit.io/fx30u3wgcqib/Screenshot%202024-01-18%20124209_v5n4oEu5V.png?updatedAt=1705578827046",
   title = "home",
 }) => {
   const router = useRouter();
