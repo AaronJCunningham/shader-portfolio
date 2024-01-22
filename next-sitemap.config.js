78f450381c665775a88b8a9e1b9a468f1df6a927
aaron-montra <aaron@montra.co>
2024-01-22 14:03:50 +0100

    next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+const config = {
+  siteUrl: "https://aaronjcunningham.com/",
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  exclude: "*/header",
+  // ...other options
+};
+
+module.exports = config;
