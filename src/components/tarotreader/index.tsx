6dac99afd409a6eb8990d631fcab03ea9a5c488d
aaron-montra <aaron@montra.co>
2024-06-03 17:32:51 +0200

    about

--- src/components/tarotreader/index.tsx
+++ src/components/tarotreader/index.tsx
@@ -60,7 +60,7 @@ export default function TarotReader() {
             your past, present, and future. Unveil the truths hidden in the
             symbols as you tread the path of discovery. Made by{" "}
             <a
-              href="https://www.aaronjcunningham.com"
+              href="https://www.aaronjcunningham.com/about"
               target="_blank"
               rel="noreferrer"
             >
