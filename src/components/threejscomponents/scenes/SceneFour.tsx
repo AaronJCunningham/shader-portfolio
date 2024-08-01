745b128940ae20bbd4a03c4d8d9457dfc2590296
aaron-montra <aaron@montra.co>
2024-08-01 11:59:41 +0200

    dark mode 3 4

--- src/components/threejscomponents/scenes/SceneFour.tsx
+++ src/components/threejscomponents/scenes/SceneFour.tsx
@@ -1,56 +1,62 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
-import * as THREE from 'three';
-import { Cloud, Clouds, Environment, MeshDistortMaterial, Sky, Stars } from '@react-three/drei';
+import * as THREE from "three";
+import {
+  Cloud,
+  Clouds,
+  Environment,
+  MeshDistortMaterial,
+  Sky,
+  Stars,
+} from "@react-three/drei";
 
-import vertexShader from "../shaders/displexRing/vertex"
-import fragmentShader from "../shaders/displexRing/fragment"
+import vertexShader from "../shaders/displexRing/vertex";
+import fragmentShader from "../shaders/displexRing/fragment";
 
-import {Dragon }from "../meshes/Dragon"
-import {Island }from "../meshes/Island"
-import {Tavern }from "../meshes/Tavern"
+import { Dragon } from "../meshes/Dragon";
+import { Island } from "../meshes/Island";
+import { Tavern } from "../meshes/Tavern";
 
-const SceneFour = ({pointer}: any) => {
+const SceneFour = ({ pointer }: any) => {
   const shaderRef = useRef<THREE.ShaderMaterial>(null);
   const meshRef = useRef<THREE.Mesh>(null);
-  const { size, clock,  } = useThree();
-
-
+  const { size, clock } = useThree();
 
   const animate = () => {
-    
     const time = clock.getElapsedTime();
-    
+
     if (shaderRef.current) {
-  
-      shaderRef.current.uniforms.uDisplace.value = time
+      shaderRef.current.uniforms.uDisplace.value = time;
       // shaderRef.current.parent[0].rotation.x = Math.sin(delta)
     }
-    if(meshRef.current){
-      meshRef.current.rotation.y += 0.2
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.2;
     }
-    
+
     requestAnimationFrame(animate);
   };
 
-useEffect(() =>{
-  animate()
-},[])
+  useEffect(() => {
+    animate();
+  }, []);
 
   return (
     <>
-   
-    <Tavern pointer={pointer}/>
+      <Tavern pointer={pointer} />
       {/* <Dragon pointer={pointer}/> */}
-      <color attach="background" args={['#001100']} />
-     <Sky distance={100} inclination={-1} sunPosition={[5,5,10]}/>
-     <ambientLight intensity={5} />
-     
-      <Environment preset='park' />
-      {/*@ts-ignore */}
-    <spotLight color="red" lookAt={new THREE.Vector3(0,-1,-2.2)} intensity={100}/>
-    {/*@ts-ignore */}
-    <spotLight color="blue" lookAt={new THREE.Vector3(0,1,-2.2)} intensity={10}/>
+      <color attach="background" args={["#226666"]} />
+      {/* <Sky distance={100} inclination={-1} sunPosition={[5,5,10]}/> */}
+
+      <ambientLight intensity={5} />
+
+      <Environment preset="park" />
+
+      <spotLight
+        color="blue"
+        //@ts-ignore
+        lookAt={new THREE.Vector3(0, 1, -2.2)}
+        intensity={10}
+      />
     </>
   );
 };
--- src/components/threejscomponents/scenes/SceneThree.tsx
+++ src/components/threejscomponents/scenes/SceneThree.tsx
@@ -1,7 +1,7 @@
-import { InstancedMesh, MathUtils, Mesh } from 'three'
-import { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Instances, Instance, Environment } from '@react-three/drei'
+import { InstancedMesh, MathUtils, Mesh } from "three";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Instances, Instance, Environment } from "@react-three/drei";
 
 interface BubbleProps {
   factor: number;
@@ -15,61 +15,87 @@ interface BubblesProps {
   pointer: { x: number; y: number };
 }
 
-
 const particles = Array.from({ length: 150 }, () => ({
   factor: MathUtils.randInt(20, 100),
   speed: MathUtils.randFloat(0.01, 0.75),
   xFactor: MathUtils.randFloatSpread(40),
   yFactor: MathUtils.randFloatSpread(10),
-  zFactor: MathUtils.randFloatSpread(10)
-}))
+  zFactor: MathUtils.randFloatSpread(10),
+}));
 
-export default function App({pointer}: any) {
+export default function App({ pointer }: any) {
   return (
- <>
-      <color attach="background" args={['#2f0f88']} />
-      <fog attach="fog" args={['#ff22ff', 20, -5]} />
+    <>
+      <color attach="background" args={["#444444"]} />
+      <fog attach="fog" args={["#ff22ff", 20, -5]} />
       <ambientLight intensity={1.5} />
       <pointLight position={[10, 10, 10]} intensity={1} castShadow />
-      <Bubbles pointer={pointer}/>
-    
+      <Bubbles pointer={pointer} />
+
       <Environment preset="city" />
-      </>
-  )
+    </>
+  );
 }
 
-function Bubbles({pointer}: BubblesProps) {
+function Bubbles({ pointer }: BubblesProps) {
   const ref = useRef<any>(null);
-  useFrame((state, delta) =>  {
-    if(!ref.current)return;
-    ref.current.rotation.y = MathUtils.damp(ref.current.rotation.y, (pointer.x * Math.PI) / 3, 2.75, delta);
-    ref.current.rotation.x = MathUtils.damp(ref.current.rotation.x, (-pointer.y * Math.PI) / 3, 2.75, delta);
-  
-  })
+  useFrame((state, delta) => {
+    if (!ref.current) return;
+    ref.current.rotation.y = MathUtils.damp(
+      ref.current.rotation.y,
+      (pointer.x * Math.PI) / 3,
+      2.75,
+      delta
+    );
+    ref.current.rotation.x = MathUtils.damp(
+      ref.current.rotation.x,
+      (-pointer.y * Math.PI) / 3,
+      2.75,
+      delta
+    );
+  });
   return (
-    <Instances limit={particles.length} ref={ref} castShadow receiveShadow position={[0, 2.5, -15]}>
+    <Instances
+      limit={particles.length}
+      ref={ref}
+      castShadow
+      receiveShadow
+      position={[0, 2.5, -15]}
+    >
       <torusKnotGeometry args={[1, 0.35, 100, 16]} />
       <meshNormalMaterial />
       {particles.map((data, i) => (
         <Bubble key={i} {...data} />
       ))}
     </Instances>
-  )
+  );
 }
 
 function Bubble({ factor, speed, xFactor, yFactor, zFactor }: BubbleProps) {
-  const ref = useRef<Mesh>()
+  const ref = useRef<Mesh>();
   useFrame((state, delta) => {
-    if(!ref.current) return;
-    const t = factor + state.clock.elapsedTime * (speed / 2)
-    ref.current.scale.setScalar(Math.max(0.5, Math.cos(t) * 1))
+    if (!ref.current) return;
+    const t = factor + state.clock.elapsedTime * (speed / 2);
+    ref.current.scale.setScalar(Math.max(0.5, Math.cos(t) * 1));
     ref.current.position.set(
-      Math.cos(t) + Math.sin(t * 1) / 10 + xFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 1) * factor) / 10,
-      Math.sin(t) + Math.cos(t * 2) / 10 + yFactor + Math.sin((t / 10) * factor) + (Math.cos(t * 2) * factor) / 10,
-      Math.sin(t) + Math.cos(t * 2) / 10 + zFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 3) * factor) / -10
-    )
-    ref.current.rotation.y = Math.sin(delta)
-    ref.current.rotation.x = Math.sin(delta)
-  })
-  return <Instance ref={ref} />
+      Math.cos(t) +
+        Math.sin(t * 1) / 10 +
+        xFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 1) * factor) / 10,
+      Math.sin(t) +
+        Math.cos(t * 2) / 10 +
+        yFactor +
+        Math.sin((t / 10) * factor) +
+        (Math.cos(t * 2) * factor) / 10,
+      Math.sin(t) +
+        Math.cos(t * 2) / 10 +
+        zFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 3) * factor) / -10
+    );
+    ref.current.rotation.y = Math.sin(delta);
+    ref.current.rotation.x = Math.sin(delta);
+  });
+  return <Instance ref={ref} />;
 }
