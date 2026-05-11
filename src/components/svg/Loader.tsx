c11bbe72f8f5d37528f5356bb0b4828d272793d3
aaronjcunningham <aaronjosephcunningham@gmail.com>
2026-05-11 14:45:25 +0200

    new loader

--- src/components/svg/Loader.tsx
+++ src/components/svg/Loader.tsx
@@ -25,32 +25,58 @@ const Loader = () => {
         viewBox="0 0 1000 1000"
         preserveAspectRatio="xMidYMid slice"
       >
-        {/* Large diamond */}
         <polygon
           points="500,80 920,500 500,920 80,500"
           className="css-loader__shape css-loader__shape--1"
         />
-        {/* Rotated inner diamond */}
         <polygon
           points="500,200 800,500 500,800 200,500"
           className="css-loader__shape css-loader__shape--2"
         />
-        {/* Tall narrow diamond */}
         <polygon
           points="500,50 650,500 500,950 350,500"
           className="css-loader__shape css-loader__shape--3"
         />
-        {/* Angular lines */}
-        <line x1="100" y1="150" x2="500" y2="500" className="css-loader__shape css-loader__shape--4" />
-        <line x1="900" y1="150" x2="500" y2="500" className="css-loader__shape css-loader__shape--4" />
-        <line x1="100" y1="850" x2="500" y2="500" className="css-loader__shape css-loader__shape--4" />
-        <line x1="900" y1="850" x2="500" y2="500" className="css-loader__shape css-loader__shape--4" />
+        <line
+          x1="100"
+          y1="150"
+          x2="500"
+          y2="500"
+          className="css-loader__shape css-loader__shape--4"
+        />
+        <line
+          x1="900"
+          y1="150"
+          x2="500"
+          y2="500"
+          className="css-loader__shape css-loader__shape--4"
+        />
+        <line
+          x1="100"
+          y1="850"
+          x2="500"
+          y2="500"
+          className="css-loader__shape css-loader__shape--4"
+        />
+        <line
+          x1="900"
+          y1="850"
+          x2="500"
+          y2="500"
+          className="css-loader__shape css-loader__shape--4"
+        />
       </svg>
 
       <div className="css-loader__content">
-        <h2 className="css-loader__title">AARON J. CUNNINGHAM</h2>
-        <p className="css-loader__label">LOADING</p>
-        <div className="css-loader__line" />
+        <div className="css-loader__text">
+          <h2 className="css-loader__title">AARON J. CUNNINGHAM</h2>
+          <p className="css-loader__label">LOADING</p>
+        </div>
+        <div className="css-loader__dots">
+          <span className="css-loader__dot" />
+          <span className="css-loader__dot" />
+          <span className="css-loader__dot" />
+        </div>
       </div>
     </div>
   );
