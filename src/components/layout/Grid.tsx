2604e07e4306fd10655860e0190e56d0a39194a3
AaronJCunningham <aaronjosephcunningham@gmail.com>
2026-05-21 17:48:36 +0200

    removed filter from grid

--- src/components/layout/Grid.tsx
+++ src/components/layout/Grid.tsx
@@ -1,7 +1,6 @@
-import { useState, FC } from "react";
+import { FC } from "react";
 import { GridItem } from "./GridItem";
 import { CV } from "./CV";
-import { useSelect } from "../../store";
 
 interface Post {
   slug: string;
@@ -14,19 +13,6 @@ interface GridProps {
 }
 
 export const Grid: FC<GridProps> = ({ posts }) => {
-  const [select, setSelect] = useSelect((state) => [
-    state.select,
-    state.setSelect,
-  ]);
-
-  const newPosts = posts.filter((post) => {
-    if (select === null) {
-      return true;
-    } else {
-      return post.categories[0] === select;
-    }
-  });
-
   return (
     <div className="project-grid" id="grid">
       <CV />
@@ -34,37 +20,8 @@ export const Grid: FC<GridProps> = ({ posts }) => {
         <span className="section-meta__index">{"//06"}</span>
         <span className="section-meta__label">MY WORK</span>
       </div>
-      <div className="bio-links">
-        <button
-          className={`bio-links__filter ${select === 5 ? "active" : ""}`}
-          onClick={() => setSelect(5)}
-        >
-          PROJECTS
-        </button>
-        <span className="bio-links__separator">|</span>
-        <button
-          className={`bio-links__filter ${select === 4 ? "active" : ""}`}
-          onClick={() => setSelect(4)}
-        >
-          NEWS
-        </button>
-        <span className="bio-links__separator">|</span>
-        <button
-          className={`bio-links__filter ${select === 8 ? "active" : ""}`}
-          onClick={() => setSelect(8)}
-        >
-          TUTORIALS
-        </button>
-        <span className="bio-links__separator">|</span>
-        <button
-          className={`bio-links__filter ${select === null ? "active" : ""}`}
-          onClick={() => setSelect(null)}
-        >
-          ALL
-        </button>
-      </div>
       <div className="main-grid-container">
-        {newPosts.map((post, index) => {
+        {posts.map((post, index) => {
           return <GridItem post={post} key={post?.slug} priority={index < 3} />;
         })}
       </div>
