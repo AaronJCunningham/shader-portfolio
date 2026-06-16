41e5a402f292d37f5c54ae8038c3f4198fd403fc
AaronJCunningham <aaronjosephcunningham@gmail.com>
2026-06-16 14:07:55 +0200

    portal thing

--- src/components/layout/PortalMenu.tsx
+++ src/components/layout/PortalMenu.tsx
@@ -44,29 +44,6 @@ export default function PortalMenu() {
 
       <div className="portal-menu__overlay" aria-hidden={!isOpen}>
         <nav className="portal-menu__panel" aria-label="Primary navigation">
-          <svg
-            className="portal-menu__plate"
-            viewBox="0 0 1000 420"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <path
-              className="portal-menu__plate-fill"
-              d="M72 28 H314 L350 62 H650 L686 28 H928 L972 72 V166 L946 198 V332 L902 392 H98 L54 348 V242 L28 210 V72 Z"
-            />
-            <path
-              className="portal-menu__plate-line"
-              d="M72 28 H314 L350 62 H650 L686 28 H928 L972 72 V166 L946 198 V332 L902 392 H98 L54 348 V242 L28 210 V72 Z"
-            />
-            <path
-              className="portal-menu__plate-detail"
-              d="M94 82 H278 M722 82 H906 M94 338 H244 M756 338 H906"
-            />
-            <path
-              className="portal-menu__plate-detail portal-menu__plate-detail--dim"
-              d="M112 112 H194 V128 H112 Z M806 112 H888 V128 H806 Z M112 292 H184 V308 H112 Z M816 292 H888 V308 H816 Z"
-            />
-          </svg>
           <div className="portal-menu__links">
             {menuItems.map((item) => {
               const isExternal = item.href.startsWith("mailto:");
